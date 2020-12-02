29478f798ea0a9ac45e4825e327a8daa99b5a2fb
dalelangfordfleming <dale.langford@flemingcollege.ca>
2020-12-01 19:18:54 -0500

    D

--- javascript.js
+++ javascript.js
@@ -9,3 +9,14 @@ $( function() {
   } );
 
   // End Owner: Dale Langford
+
+  //Test java for navbar
+  function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
